c44e104d198054200eab3a8132fa3331bf0dbda0
godheeran <heeran3646@gmail.com>
2021-02-17 17:23:13 +0900

    5.1 Apply the Bootstrap API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
 import "./App.css";
+import { Container } from "react-bootstrap";
 
 class App extends React.Component {
   state = {
@@ -16,6 +17,7 @@ class App extends React.Component {
         data: {movies}
       }
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+
     this.setState({movies, isLoading: false});
   }
 
@@ -28,27 +30,52 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
     console.log(movies)
     return (
-      <section className="container">
+      <Container>
         {isLoading ? (
           <div className="loader">
             <span className="loader_text">Loading...</span>
           </div>
         ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
+            <div className="movies">
+              <div className="row">
+                {movies.map(movie => (
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+      </Container>
+      // <section className="container">
+      //   {isLoading ? (
+      //     <div className="loader">
+      //       <span className="loader_text">Loading...</span>
+      //     </div>
+      //   ) : (
+      //     <div className="movies">
+      //         <div className="row">
+      //           {movies.map(movie => (
+      //             <Movie
+      //               key={movie.id}
+      //               id={movie.id}
+      //               year={movie.year}
+      //               title={movie.title}
+      //               summary={movie.summary}
+      //               poster={movie.medium_cover_image}
+      //               genres={movie.genres}
+      //             />
+      //           ))}
+      //         </div>
+      //     </div>
+      //   )}
+      // </section>
     );
   }
   
--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Col } from "react-bootstrap";
 
 function Movie({ year, title, summary, poster, genres }){
     return (
-        <div className="movie">
+        <Col className="movie" md={4} lg={6}>
             <img src={poster} alt={title}></img>
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
@@ -13,14 +14,18 @@ function Movie({ year, title, summary, poster, genres }){
                 {/* We are doing that because sometimes genres is undefined.
                 If you call .map on undefined you will get an error :) */}
                 {genres && genres.map((genre, index) => (
-                    <li key={index} className="genres__genre">
-                    {genre}
-                    </li>
+                    index < 3 ? (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ) : (
+                        <p></p>
+                    )
                 ))}
                 </ul>
                 <p className="movie__summary">{summary.slice(0, 240)}...</p>
             </div>
-        </div>
+        </Col>
     );
 }
 
